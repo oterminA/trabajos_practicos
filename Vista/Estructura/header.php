bf5203f5b7285d6406692737b29789f4c8459acf
oterminA <otermincarolina98@gmail.com>
2025-10-05 13:18:38 -0300

    Agregué el menú de los demás tps.

--- Vista/Estructura/header.php
+++ Vista/Estructura/header.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" href="/trabajos_practicos/Vista/Css/styleEstructura.css">
 
 <header>
-    <h1>Trabajo práctico n° 4 PWD 2025</h1>
+    <h1>Trabajo prácticos PWD 2025</h1>
     <!-- cambiar el titulo despues, cuando se muestre la totalidad de los tps -->
     <nav>
         <!-- Se comentan los otros enlaces a los ejercicios pasados xra que solo muestre el tp4 -->
          <ul class="menu-principal">
-          <!--  <li class="elemento-con-submenu">
+           <li class="elemento-con-submenu">
                 <a href="#" class="boton-desplegable">TP1</a>
                 <ul class="contenido-desplegable">
                     <li><a href="/trabajos_practicos/Vista/TP1/ejercicio_1.php">Ejercicio 1</a></li>
@@ -23,8 +23,6 @@
             <li class="elemento-con-submenu">
                 <a href="#" class="boton-desplegable">TP2</a>
                 <ul class="contenido-desplegable">
-                    <li><a href="../TP2/ejercicio_1.php">Ejercicio 1</a></li>
-
                     <li class="elemento-submenu">
                         <a href="#">Ejercicio 2</a>
                         <ul class="submenu-secundario">
@@ -50,7 +48,7 @@
                     <li><a href="/trabajos_practicos/Vista/TP3/ejercicio_2.php">Ejercicio 2</a></li>
                     <li><a href="/trabajos_practicos/Vista/TP3/ejercicio_3.php">Ejercicio 3</a></li>
                 </ul>
-            </li> -->
+            </li>
 
             <li class="elemento-con-submenu">
                 <a href="#" class="boton-desplegable">TP4</a>
